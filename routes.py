608ae2a5b1f4b78f70c40269b17b9059eca157f5
Michael Menjivar Alvarado <michaelmenjivar456@gmail.com>
2024-12-19 08:51:51 -0900

    Updated routes to run addtemplate

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, redirect, url_for
+from app import app, db
+from models import MovieReview
 
 @app.route('/')
 def index():
@@ -8,3 +9,19 @@ def index():
 @app.route('/data')
 def data():
     return render_template('data.html')
+
+@app.route('/add_review', methods=['GET', 'POST'])
+def add_review():
+    if request.method == 'POST':
+        new_review = MovieReview(
+            movie_name=request.form['movie_name'],
+            review=request.form['review'],
+            rating=float(request.form['rating'])
+        )
+        
+        db.session.add(new_review)
+        db.session.commit()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('add_review.html')
